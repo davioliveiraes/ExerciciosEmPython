5047197a183f35eb587410dd128683cac88fe1a6
Davi <davioliveiraes7@gmail.com>
2025-01-26 19:15:59 -0300

    Adding exercise

--- felizNatal.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def feliz_natal():
-    ano_atual = datetime.date.today().year
-    dia_natal = datetime.date(ano_atual, 12, 25)
-    return f'{dia_natal.day}/{dia_natal.month}/{dia_natal.year} - FELIZ NATAL!'
-
-
-print(feliz_natal())
