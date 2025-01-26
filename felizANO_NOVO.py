82c263a148a0819d4260386f331a975ce4b102ac
Davi <davioliveiraes7@gmail.com>
2025-01-26 19:15:19 -0300

    Deleted exercise

--- felizANO_NOVO.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def feliz_ano_novo():
-    ano_atual = datetime.date.today().year
-    data_ano_novo = datetime.date(ano_atual, 1, 1)
-    return data_ano_novo.strftime('%d/%m/%Y' + ' - FELIZ ANO NOVO!')
-
-print(feliz_ano_novo())
